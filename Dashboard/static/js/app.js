21acc8846607431dde526328e4e70c3e2733ea6a
hbostanchi <42923135+hbostanchi@users.noreply.github.com>
2020-03-24 16:40:37 -0700

    map

--- Dashboard/static/js/app.js
+++ Dashboard/static/js/app.js
@@ -1,7 +1,7 @@
 // import the data from data.js
 const tableData = data;
-
-
+// API key
+const API_KEY = "pk.eyJ1IjoiaGJvc3RhbmNoaSIsImEiOiJjazUyeXdqNXgwMzM2M21udHBydGJ4OHllIn0.VJBG1JKQpfECTt3vM5Qgxw";
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
